8fc1ab1c8571bd8ace75e7f03ec1747b667d336d
Krishna Girkar <girkar.km@gmail.com>
2026-06-16 00:35:40 -0700

    use system python for this, same as shared mimir config

--- setup.sh
+++ setup.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
-curl -LsSf https://astral.sh/uv/install.sh | env UV_NO_MODIFY_PATH=1 sh
-
-ANSIBLE_VENV="$HOME/.local/ansible-venv"
-uv venv --python 3.14.4 "$ANSIBLE_VENV"
-uv pip install ansible==13.6.0 --python "$ANSIBLE_VENV/bin/python"
+ANSIBLE_VENV="/opt/ansible-venv"
+sudo python3 -m venv --system-site-packages "$ANSIBLE_VENV"
+sudo "$ANSIBLE_VENV/bin/pip" install ansible==13.6.0

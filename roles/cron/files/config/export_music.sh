d08afcaea68a9c5f1429185e5d375bf05cf589a5
Krishna Girkar <girkar.km@gmail.com>
2026-06-18 22:28:59 -0700

    fix beet musiclist export

--- roles/cron/files/config/export_music.sh
+++ roles/cron/files/config/export_music.sh
@@ -3,4 +3,4 @@ export BEETSDIR="$HOME/.config/beets"
 BEET="$HOME/.local/bin/beet"
 MUSICLIST="/mnt/nas/share/k9/Music/musiclist.csv"
 
-"$BEET" export -all -format=csv -output="$MUSICLIST" --include-keys="albumartist,album,year,genre"
+"$BEET" export --album --format=csv --output="$MUSICLIST" --include-keys="albumartist,album,year,genre"
